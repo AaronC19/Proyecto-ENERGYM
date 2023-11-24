18be61bc7d12ded6dd3f3e2041b5bb04213c4a41
AaronC19 <125511809+AaronC19@users.noreply.github.com>
2023-11-24 06:04:10 -0600

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,3 +1,51 @@
+<?php
+// Establecer la conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "aaronCR14";
+$dbname = "inicioderegistro";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recuperar datos del formulario y evitar inyección SQL
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['correo']);
+    $password = mysqli_real_escape_string($conn, $_POST['contrasena']);
+    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirmar']);
+    $user_type = mysqli_real_escape_string($conn, $_POST['tipo_usuario']);
+
+    // Verificar que el tipo de usuario sea "cliente" antes de continuar
+    if ($user_type !== "cliente") {
+        echo "Error: Solo se permiten registros como clientes.";
+        exit();
+    }
+
+    // Hash de la contraseña (puedes usar algoritmos más seguros según tus necesidades)
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Realizar la inserción en la base de datos
+    $insert_query = "INSERT INTO usuarios (nombre_usuario, correo, contrasena, tipo_usuario) VALUES ('$username', '$email', '$hashed_password', '$user_type')";
+
+    if ($conn->query($insert_query) === TRUE) {
+        // Registro exitoso, redirige a index.php
+        header("Location: index.php");
+        exit(); // Asegura que no se ejecute más código después de la redirección
+    } else {
+        echo "Error al registrar el usuario: " . $conn->error;
+    }
+}
+
+// Cierra la conexión al final del archivo si ya no la necesitas
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,28 +71,25 @@
                         <h3 class="text-center">Inscripción ENERGYM</h3>
                     </div>
                     <div class="card-body p-5">
-                        <form id="registro-form">
+                        <form id="registro-form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                             <div class="form-group">
                                 <label for="username">Nombre de Usuario</label>
-                                <input type="text" class="form-control" id="username" required />
+                                <input type="text" class="form-control" name="username" id="username" required />
                             </div>
                             <div class="form-group">
                                 <label for="correo">Correo Electrónico</label>
-                                <input type="email" class="form-control" id="correo" required />
+                                <input type="email" class="form-control" name="correo" id="correo" required />
                             </div>
                             <div class="form-group">
                                 <label for="contrasena">Contraseña</label>
-                                <input type="password" class="form-control" id="contrasena" required />
+                                <input type="password" class="form-control" name="contrasena" id="contrasena" required />
                             </div>
                             <div class="form-group">
                                 <label for="confirmar">Confirmar Contraseña</label>
-                                <input type="password" class="form-control" id="confirmar" required />
+                                <input type="password" class="form-control" name="confirmar" id="confirmar" required />
                             </div>
-                            <label for="tipo_usuario">Tipo de Usuario:</label>
-                            <select id="tipo_usuario" name="tipo_usuario" required>
-                                <option value="cliente">Cliente</option>
-                                <option value="administrador">Administrador</option>
-                            </select>
+                            <!-- Tipo de usuario fijo como "cliente" -->
+                            <input type="hidden" name="tipo_usuario" value="cliente">
                             <button type="submit" class="btn btn-block text-white btn-registro mt-3">
                                 Registrarse
                             </button>
